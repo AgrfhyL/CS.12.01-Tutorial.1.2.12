82e2dd5c79d09204e4c02b2a87d188bff17ae732
agrfhyl <andyl2026@student.cis.edu.hk>
2024-08-22 14:59:15 +0800

    Hi

---
+++ src/Successors.java
@@ -0,0 +1,25 @@
+public class Successors {
+
+
+    public static Position findPosition(int num, int[][] arr) {
+        Position out = null;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == num) {
+                    out = new Position(i, j);
+                }
+            }
+        }
+        return out;
+    }
+
+    public static Position[][] getSuccessorArray(int[][] arr) {
+        Position[][] out = new Position[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                out[i][j] = findPosition(arr[i][j] + 1, arr);
+            }
+        }
+        return out;
+    }
+}
